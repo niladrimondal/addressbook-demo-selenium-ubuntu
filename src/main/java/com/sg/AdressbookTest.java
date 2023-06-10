c9eb63eb6c372fbed04595a4f5c7fdab9cb64f29
NiladriSMondal <niladrisankarmondal.mondal@gmail.com>
2023-06-10 21:22:42 +0530

    initial commit

---
+++ src/main/java/com/sg/AdressbookTest.java
@@ -0,0 +1,59 @@
+package com.sg;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class AdressbookTest {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		//System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "/home/ubuntu/chromedriver");
+		
+		ChromeOptions co = new ChromeOptions();
+		
+		WebDriver wd = new ChromeDriver(co);
+		co.addArguments("--headless");
+		co.addArguments("--no-sandbox");
+        co.addArguments("--disable-dev-shm-usage");
+    	co.addArguments("--remote-allow-origins=*");
+		
+		wd.get("http://52.53.225.73:8081/addressbooksc/");
+		//wd.get("https://www.flipkart.com/");
+		System.out.println("Test Execution Started");
+		wd.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
+		
+		//For Finding the button New COntact
+		wd.findElement(By.className("v-button")).click();
+		
+		//Enter The First name
+		wd.findElement(By.id("gwt-uid-5")).sendKeys("Niladri");
+		
+		//Enter the Last Name
+		wd.findElement(By.id("gwt-uid-7")).sendKeys("Mondal");
+		
+		//Phone Number
+		wd.findElement(By.id("gwt-uid-9")).sendKeys("999999999");
+		
+		//Email
+		wd.findElement(By.id("gwt-uid-11")).sendKeys("nilari@gmail.com");
+		
+		//DOB
+		wd.findElement(By.id("gwt-uid-13")).sendKeys("6/10/23,");
+		
+	    Thread.sleep(3000);
+	    
+	    wd.findElement(By.className("v-button-primary")).click();
+	    
+	    Thread.sleep(2000);
+	    
+	    System.out.println("Test Execution is successfull");
+		wd.quit();
+
+	}
+
+}
