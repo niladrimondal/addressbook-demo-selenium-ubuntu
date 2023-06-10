7392fcdc00b599d1c07acadc9f53c131876060fd
NiladriSMondal <niladrisankarmondal.mondal@gmail.com>
2023-06-11 00:54:40 +0530

    initial commit

--- src/main/java/com/sg/AdressbookTest.java
+++ src/main/java/com/sg/AdressbookTest.java
@@ -12,7 +12,7 @@ public class AdressbookTest {
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		//System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver.exe");
-		System.setProperty("webdriver.chrome.driver", "/home/ubuntu/chromedriver");
+		System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
 		
 		ChromeOptions co = new ChromeOptions();
 		
@@ -23,6 +23,7 @@ public class AdressbookTest {
 		co.addArguments("--disable-gpu"); // applicable to windows os only
 		co.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
 		co.addArguments("--no-sandbox"); // Bypass OS security model
+		co.addArguments("--headless");
         WebDriver wd = new ChromeDriver(co);
 		wd.get("http://52.53.225.73:8081/addressbooksc/");
 		//wd.get("https://www.flipkart.com/");
